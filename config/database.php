ec1df9d2f5ee21fc5c379403607e476148a0949d
Karel van Zijl <sonoind@gmail.com>
2021-06-17 21:21:25 +0800

    [FEATURE] connect to external Redis server

--- config/database.php
+++ config/database.php
@@ -123,6 +123,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
@@ -131,6 +132,7 @@ return [
         ],
 
         'cache' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
